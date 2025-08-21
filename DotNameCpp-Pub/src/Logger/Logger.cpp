aa98666a3b709686ef92a5298b37809a47928739
tomasmark79 <44719504+tomasmark79@users.noreply.github.com>
2025-08-21 14:14:23 +0000

    🔄 Auto-sync from private repository
    Source Commit: https://github.com/tomasmark79/DotNameCpp/commit/1c2b23501bd6372525fbfa7620dda65c48a9f479
    Files Changed: 27

--- DotNameCpp-Pub/src/Logger/Logger.cpp
+++
@@ -1,5 +0,0 @@
-// MIT License
-// Copyright (c) 2024-2025 Tomáš Mark
-// Thread safe logger class
-
-#include "Logger.hpp"
--- DotNameCpp-Pub/src/Utils/Utils.cpp
+++
@@ -1,4 +0,0 @@
-// MIT License
-// Copyright (c) 2024-2025 Tomáš Mark
-
-#include "Utils.hpp"
